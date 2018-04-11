3458ff5bccc8dec5401b9f15ab0b8d01a8aeefa0
pttx <jacek.kusmierczyk@wp.pl>
2018-04-11 20:38:53 +0200

    add algorithm calculating difference of 2 parabolic integrals

---
+++ 2_parabole.py
@@ -0,0 +1,24 @@
+from calkaOznaczona import *
+
+def roznica_funkcji(a1,a2,a3,b1,b2,b3):
+    return a1-b1, a2-b2, a3-b3
+
+
+def pole(a1,a2,a3,b1,b2,b3):
+    a, b, c = roznica_funkcji(a1,a2,a3,b1,b2,b3) 
+
+    sqrt_delta = ((b**2) - (4*a*c))**(1/2)
+
+    if(sqrt_delta <= 0 or a==0):
+        return 0
+
+    x1 = ((-b - sqrt_delta)/(2 *a))
+    x2 = ((-b + sqrt_delta)/(2 *a))
+
+    def f(x):
+        return((a * (x**2)) + (b*x) + c)
+
+    return abs(calka(f,x1,x2,1000))
+    
+
+print(pole(1,3,3,1,3,3))
---
+++ calkaOznaczona.py
@@ -0,0 +1,24 @@
+def funkcja(x):
+    return (x-2)
+
+def calka(f,a,b,ilosc_trapezow):
+
+
+
+    epsilon = (b-a)/ilosc_trapezow
+    poczatekLiczonegoPrzedzialu = a
+    koniecLiczonegoPrzedzialu = a + epsilon
+
+    suma = 0
+
+    for i in range(ilosc_trapezow):
+
+        suma += ((f(poczatekLiczonegoPrzedzialu) + f(koniecLiczonegoPrzedzialu))/2)*epsilon
+
+        poczatekLiczonegoPrzedzialu = koniecLiczonegoPrzedzialu
+        koniecLiczonegoPrzedzialu = koniecLiczonegoPrzedzialu + epsilon
+
+    return suma
+
+
+print(calka(funkcja,1,3,1))
